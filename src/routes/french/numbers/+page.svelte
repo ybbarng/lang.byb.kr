fa105c9995a14ded5227f91084fe90e2ec10be00
ybbarng <ybbarng@gmail.com>
2023-08-28 09:21:25 +0900

    Remote tailwind css to test mobile

--- src/routes/french/numbers/+page.svelte
+++ src/routes/french/numbers/+page.svelte
@@ -34,9 +34,9 @@
         La Voix
     </label>
     <div class="inline-block relative w-64">
+        <!-- class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" -->
         <select
             id="voice"
-            class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
             bind:value={selectedVoiceUri}
         >
             {#each voices as voice}
