f1defd6d7fc7d657c9589ac59b579202f6535cf9
ybbarng <ybbarng@gmail.com>
2023-08-28 09:38:24 +0900

    Make working for mobile

--- src/routes/french/numbers/NumberListeningGame.svelte
+++ src/routes/french/numbers/NumberListeningGame.svelte
@@ -1,38 +1,36 @@
 <script lang="ts">
-    export var selectedVoiceUri: String = '';
+  export var selectedVoiceUri: String = '';
 
-    var minNumber = 0;
-    var maxNumber = 100;
-    var answer = 0;
+  var minNumber = 0;
+  var maxNumber = 100;
+  var answer = 0;
 
-    const getRandomNumber = () => {
-        return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
-    };
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+  };
 
-    const getVoice = () => {
-        return speechSynthesis.getVoices()
-            .filter((voice) => voice.voiceURI == selectedVoiceUri)[0];
-    }
+  const getVoice = () => {
+    return speechSynthesis.getVoices().filter((voice) => voice.voiceURI == selectedVoiceUri)[0];
+  };
 
-    const play = () => {
-        answer = getRandomNumber();
-        const voice = getVoice();
-        const utter = new SpeechSynthesisUtterance(answer.toString());
-        utter.voice = voice
-        speechSynthesis.speak(utter);
-
-    };
+  const play = () => {
+    answer = getRandomNumber();
+    const voice = getVoice();
+    const utter = new SpeechSynthesisUtterance(answer.toString());
+    utter.voice = voice;
+    speechSynthesis.speak(utter);
+  };
 </script>
 
 <svelte:head>
-    <title>Les Nombres en français</title>
-    <meta name="description" content="Étudions les chiffres français." />
+  <title>Les Nombres en français</title>
+  <meta name="description" content="Étudions les chiffres français." />
 </svelte:head>
 
 <section>
-    <div class="inline-block relative w-64">
-        <button class="btn btn-blue" on:click={play}> Button </button>
-    </div>
+  <div class="inline-block relative w-64">
+    <button class="btn btn-blue" on:click={play}> Button </button>
+  </div>
 </section>
 
 <style>
